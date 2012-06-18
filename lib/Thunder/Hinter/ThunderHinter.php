196aeed69aab8752480a8b6f152082e3e04f6783
Tomasz Kowalczyk <tomasz@kowalczyk.cc>
2012-06-18 23:29:43 +0200

    added PHPDoc for TypeHinter class methods and attributes

--- lib/Thunder/Hinter/ThunderHinter.php
+++ lib/Thunder/Hinter/ThunderHinter.php
@@ -4,20 +4,29 @@ namespace Thunder\Hinter;
 /**
  * Main library class expected to be injected into target objects and use hooks
  * in their __call() method.
+ *
+ * @author Tomasz Kowalczyk <tomasz@kowalczyk.cc>
  */
 class ThunderHinter
     {
     /**
-     * @var array
+     * @var array Overloaded class method calls metadata
      */
     protected $calls;
 
+    /**
+     * Constructor
+     *
+     * @param array $callsMetadata Definition of overloaded class methods
+     */
     public function __construct(array $callsMetadata = array())
         {
         $this->calls = $callsMetadata;
         }
 
     /**
+     * Returns internal representation of registered overloaded calls metadata
+     *
      * @return array
      */
     public function getCallsMetadata()
@@ -25,27 +34,37 @@ class ThunderHinter
         return $this->calls;
         }
 
-    public function resolveVariableType($variable, $resolveObjectClassName = false)
+    /**
+     * Returns type of given $variable value. May resolve object class name if
+     * second parameter equals true
+     *
+     * @param mixed $variable Value to process
+     * @param bool $getClassName If type == 'object' decides whether return such value or its class name
+     *
+     * @see get_class()
+     *
+     * @return string Value type name
+     */
+    public function resolveVariableType($variable, $getClassName = false)
         {
         $type = strtolower(gettype($variable));
-        if('object' == $type && $resolveObjectClassName)
+        if('object' == $type && $getClassName)
             {
             $type = get_class($variable);
             }
         return $type;
         }
 
-    /* public function resolveArgumentTypes(array $arguments, $resolveObjectClassName = false)
-        {
-        $ret = array();
-        foreach($arguments as $argument)
-            {
-            $ret[] = $this->resolveVariableType($argument, $resolveObjectClassName);
-            }
-        return $ret;
-        } */
-
-    public function isValidCall(array $args, array $callMetadata, $resolveObjectClassName = false)
+    /**
+     * Validates given argument set against passed call option metadata
+     *
+     * @param array $args Set of arguments
+     * @param array $callMetadata Call option metadata
+     * @param bool $getClassName Resolve objects class names?
+     *
+     * @return bool Whether arguments match call option or not
+     */
+    public function isValidCall(array $args, array $callMetadata, $getClassName = false)
         {
         $callMetadataCount = count($callMetadata);
         if(count($args) != $callMetadataCount)
@@ -54,7 +73,7 @@ class ThunderHinter
             }
         for($i = 0; $i < $callMetadataCount; $i++)
             {
-            if(!$this->isValidArgument($args[$i], $callMetadata[$i], $resolveObjectClassName))
+            if(!$this->isValidArgument($args[$i], $callMetadata[$i], $getClassName))
                 {
                 return false;
                 }
@@ -62,14 +81,32 @@ class ThunderHinter
         return true;
         }
 
-    public function isValidArgument($argument, $expectedType, $resolveObjectClassName)
+    /**
+     * Validates single argument against expected type metadata
+     *
+     * @param mixed $argument Value to process
+     * @param mixed $expectedType Expected type from call metadata
+     * @param boolean $getClassName Resolve objects class names?
+     *
+     * @return bool Whether argument match expected type or not
+     */
+    public function isValidArgument($argument, $expectedType, $getClassName)
         {
-        $argumentType = $this->resolveVariableType($argument, $resolveObjectClassName);
+        $argumentType = $this->resolveVariableType($argument, $getClassName);
         return
             !(is_array($expectedType) && !in_array($argumentType, $expectedType)
             || (!is_array($expectedType) && $expectedType != $argumentType));
         }
 
+    /**
+     * Returns all overloaded calls metadata for given method name
+     *
+     * @param string $method Method name
+     *
+     * @return array Overloaded method calls metadata
+     *
+     * @throws \LogicException When method calls metadata does not exist
+     */
     public function getMethodCallsMetadata($method)
         {
         if(isset($this->calls[$method]))
@@ -79,6 +116,16 @@ class ThunderHinter
         throw new \LogicException(sprintf('There is no call metadata for method: "%s".', $method));
         }
 
+    /**
+     * Determines matching object method from method name and specified arguments
+     *
+     * @param string $method Method name
+     * @param array $args Call arguments
+     *
+     * @return string Target method name
+     *
+     * @throws \RuntimeException When there is no matching overloaded method call
+     */
     public function matchCall($method, $args)
         {
         $call = $this->getMethodCallsMetadata($method);
