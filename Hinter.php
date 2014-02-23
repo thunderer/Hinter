7c4a8dff79b8e66032921e70a07b072a93af6064
Tomasz Kowalczyk <tomasz@kowalczyk.cc>
2014-02-23 22:34:12 +0100

    major library refactoring

---
+++ Hinter.php
@@ -0,0 +1,172 @@
+<?php
+namespace Thunder\Hinter;
+
+/**
+ * ThunderHinter library main class
+ *
+ * @author Tomasz Kowalczyk <tomasz@kowalczyk.cc>
+ */
+class Hinter
+    {
+    private $methods = array();
+
+    /**
+     * Yep, it's a constructor.
+     *
+     * @param array $methods Methods calls definitions
+     *
+     * @throws \RuntimeException When method definition is invalid
+     */
+    public function __construct(array $methods)
+        {
+        foreach($methods as $name => $calls)
+            {
+            if(!is_array($calls))
+                {
+                $message = 'Invalid %s method calls metadata.';
+                throw new \RuntimeException(sprintf($message, $name));
+                }
+            $this->configureMethod($name, $calls);
+            }
+        }
+
+    /**
+     * Matches method call against given arguments array and returns target
+     * method name to call on your object
+     *
+     * @param string $method Overloaded method name
+     * @param array $args Arguments array
+     *
+     * @return mixed Target method return value
+     *
+     * @throws \RuntimeException When method or matching call was not found
+     */
+    public function match($method, array $args)
+        {
+        if(!array_key_exists($method, $this->methods))
+            {
+            $message = 'Method %s was not configured. Available: %s.';
+            $methods = implode(',', array_keys($this->methods));
+            throw new \RuntimeException(sprintf($message, $method, $methods));
+            }
+        $call = $this->methods[$method];
+        foreach($call as $targetMethod => $callMetadata)
+            {
+            if($this->isValidCall($args, $callMetadata))
+                {
+                return $targetMethod;
+                }
+            }
+
+        $message = 'Failed to match call %s!';
+        $signature = $this->getSignature($this, $method, $args);
+        throw new \RuntimeException(sprintf($message, $signature));
+        }
+
+    /**
+     * Shortcut method for implementations to use for example inside __call()
+     * method: finds target method using match(), calls it on passed object
+     * and returns its value
+     *
+     * @param object $object Instance to call methods on
+     * @param string $method Overloaded method name
+     * @param array $args Arguments array
+     *
+     * @return mixed Whatever target method returns
+     *
+     * @throws \RuntimeException If instance does not implement target method
+     */
+    public function call($object, $method, $args)
+        {
+        $target = $this->match($method, $args);
+        if(!method_exists($object, $target))
+            {
+            $message = 'Matched method %s but it does not exist!';
+            $signature = $this->getSignature($this, $target, $args);
+            throw new \RuntimeException(sprintf($message, $signature));
+            }
+
+        return call_user_func_array(array($object, $target), $args);
+        }
+
+    private function configureMethod($name, array $calls)
+        {
+        $this->methods[$name] = array();
+        foreach($calls as $target => $call)
+            {
+            if(is_array($call))
+                {
+                $this->methods[$name][$target] = $call;
+                }
+            else if(is_string($call))
+                {
+                $parts = explode(',', $call);
+                foreach($parts as $key => $part)
+                    {
+                    $parts[$key] = (false !== strpos($part, '|'))
+                        ? explode('|', $part)
+                        : $part;
+                    }
+                $this->methods[$name][$target] = $parts;
+                }
+            else
+                {
+                $message = 'Invalid %s method call definition.';
+                throw new \RuntimeException(sprintf($message, $name));
+                }
+            }
+        }
+
+    private function isValidCall(array $args, array $callMetadata)
+        {
+        $expectedArgsCount = count($callMetadata);
+        $argsCount = count($args);
+        if($argsCount != $expectedArgsCount)
+            {
+            return false;
+            }
+        for($i = 0; $i < $expectedArgsCount; $i++)
+            {
+            if(!$this->isValidArgument($args[$i], $callMetadata[$i]))
+                {
+                return false;
+                }
+            }
+
+        return true;
+        }
+
+    private function isValidArgument($value, $expected)
+        {
+        switch($expected)
+            {
+            // these types are not returned by gettype()
+            case 'numeric': { return is_numeric($value); }
+            case 'callable': { return is_callable($value); }
+            case 'object': { return is_object($value); }
+            case 'float': { return is_float($value); }
+            }
+
+        return is_array($expected)
+            ? array_reduce($expected, function($return, $item) use($value) {
+                return $return ?: $this->isValidArgument($value, $item);
+                }, false)
+            : $this->getArgumentType($value) === $expected;
+        }
+
+    private function getSignature($object, $method, array $args)
+        {
+        $types = array_map(function($item) {
+            return $this->getArgumentType($item, true);
+            }, $args);
+
+        return get_class($object).'::'.$method.'('.implode(',', $types).')';
+        }
+
+    private function getArgumentType($variable)
+        {
+        return is_object($variable)
+            ? get_class($variable)
+            : strtolower(gettype($variable));
+        }
+    }
---
+++ Tests/HinterTest.php
@@ -0,0 +1,139 @@
+<?php
+namespace Thunder\Hinter\Tests;
+
+use Thunder\Hinter\Hinter;
+
+class Hinted
+    {
+    private $state;
+    private $hinter;
+
+    public function __construct()
+        {
+        $config = array(
+            'plus' => array(
+                'plusOne' => array('numeric'),
+                'plusTwo' => array('integer', 'double'),
+                ),
+            'minus' => array(
+                'minusOne' => 'numeric',
+                'minusTwo' => 'integer,double',
+                ),
+            'invalid' => array(
+                'invalidCall' => 'null',
+                ),
+            );
+        $this->hinter = new Hinter($config);
+        $this->state = 0;
+        }
+
+    public function getState()
+        {
+        return $this->state;
+        }
+    
+    private function plusOne($arg) { $this->state += $arg; }
+    private function plusTwo($arg1, $arg2) { $this->state += ($arg1 + $arg2); }
+    private function minusOne($arg) { $this->state -= $arg; }
+    private function minusTwo($arg1, $arg2) { $this->state -= ($arg1 + $arg2); }
+
+    public function __call($method, array $args)
+        {
+        return method_exists($this, $method)
+            ? call_user_func_array(array($this, $method), $args)
+            : $this->hinter->call($this, $method, $args);
+        }
+    }
+
+class HinterTest extends \PHPUnit_Framework_TestCase
+    {
+    public function testImplementation()
+        {
+        $hinted = new Hinted();
+        $calls = array(
+            array('plus', array(0), 0),
+            array('plus', array(0, 1.5), 1.5),
+            array('minus', array(0), 1.5),
+            array('minus', array(0, 0.5), 1),
+            );
+
+        $this->assertEquals(0, $hinted->getState());
+        foreach($calls as $call)
+            {
+            call_user_func_array(array($hinted, $call[0]), $call[1]);
+            $this->assertEquals($call[2], $hinted->getState());
+            }
+
+        $this->setExpectedException('\RuntimeException');
+        call_user_func_array(array($hinted, 'invalid'), array(null));
+        }
+
+    /**
+     * @dataProvider provideMethods
+     */
+    public function testMethods($method, array $args, $target, $exception)
+        {
+        $config = array(
+            'add' => array(
+                'addOne' => array('integer'),
+                'addTwo' => array('integer', 'integer'),
+                'addThree' => array('integer', array('integer', 'double'), 'integer'),
+                ),
+            'sub' => array(
+                'subOne' => 'integer',
+                'subTwo' => 'integer,integer',
+                'subThree' => 'integer|double,integer,integer',
+                'subObject' => 'object,stdClass',
+                ),
+            'other' => array(
+                'call' => array('callable'),
+                'callOption' => array(array('numeric'), array('integer', 'Closure')),
+                ),
+            );
+        $hinter = new Hinter($config);
+        if(null !== $exception)
+            {
+            $this->setExpectedException($exception);
+            }
+        $result = $hinter->match($method, $args);
+        $this->assertEquals($target, $result);
+        }
+
+    public function provideMethods()
+        {
+        return array(
+            array('add', array(2), 'addOne', null),
+            array('add', array(2, 56), 'addTwo', null),
+            array('add', array(2, new \stdClass()), null, '\RuntimeException'),
+            array('add', array(4.45, false), null, 'exception'),
+            array('add', array(4, 4.45, 4), 'addThree', null),
+            array('add', array(4, 5, 4), 'addThree', null),
+            array('add', array(4, false, 4), null, '\RuntimeException'),
+
+            array('sub', array(4), 'subOne', null),
+            array('sub', array(new \stdClass(), new \stdClass()), 'subObject', null),
+            array('sub', array(1.5, 1, 1), 'subThree', null),
+
+            array('other', array(function() {}), 'call', null),
+            array('other', array(5.4, function() {}), 'callOption', null),
+
+            array('invalid', array(), 'invalid', '\RuntimeException'),
+            );
+        }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testInvalidMethodMetadata()
+        {
+        new Hinter(array('add' => null));
+        }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testInvalidMethodCallMetadata()
+        {
+        new Hinter(array('add' => array('addOne' => null)));
+        }
+    }
